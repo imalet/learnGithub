fe112aa0006e71fb4148660b6fb03cbdb3e54488
IMALET Benji <imaletbenji58@gmail.com>
2023-10-16 14:16:00 +0200

    Nous avons optimiser le code du switch

--- function.php
+++ function.php
@@ -7,40 +7,55 @@ function aurevoir()
 
 // aurevoir();
 
-$input = 10;
+// $input = 10;
 
-switch ($input) {
-    case 1:
-        echo 'Un';
-        break;
-    case 2:
-        echo 'Deux';
-        break;
-    case 3:
-        echo 'Trois';
-        break;
-    case 4:
-        echo 'Quatre';
-        break;
-    case 5:
-        echo 'Cinq';
-        break;
-    case 6:
-        echo 'Six';
-        break;
-    case 7:
-        echo 'Sept';
-        break;
-    case 8:
-        echo 'Huit';
-        break;
-    case 9:
-        echo 'Neuf';
-        break;
-    case 10:
-        echo 'Dix';
-        break;
-    default:
-        echo "Desole mon frere";
-        break;
+// switch ($input) {
+//     case 1:
+//         echo 'Un';
+//         break;
+//     case 2:
+//         echo 'Deux';
+//         break;
+//     case 3:
+//         echo 'Trois';
+//         break;
+//     case 4:
+//         echo 'Quatre';
+//         break;
+//     case 5:
+//         echo 'Cinq';
+//         break;
+//     case 6:
+//         echo 'Six';
+//         break;
+//     case 7:
+//         echo 'Sept';
+//         break;
+//     case 8:
+//         echo 'Huit';
+//         break;
+//     case 9:
+//         echo 'Neuf';
+//         break;
+//     case 10:
+//         echo 'Dix';
+//         break;
+//     default:
+//         echo "Desole mon frere";
+//         break;
+// }
+
+
+$tbls = [
+    1=>'un',
+    2=>'deux',
+    3=>'trois',
+    4=>'quatre',
+    5=>'cinq',
+];
+$val = 5;
+foreach ($tbls as $key => $value) {
+    if ($key == $val) {
+        echo $value;
+    }
 }
